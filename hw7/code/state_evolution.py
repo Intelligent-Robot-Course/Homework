dcb2bdee9df393626207f61bbfcc0b81c94d85f0
zsustc <843811748@qq.com>
2022-04-21 09:49:59 +0800

    Update state_evolution.py

--- hw7/code/state_evolution.py
+++ hw7/code/state_evolution.py
@@ -14,7 +14,7 @@ theta_next = theta + omega
 
 def next_state(prev_state, ip):
     x_next = prev_state[0] + ip[0] * math.acos(prev_state[2])
-    y_next = prev_state[1] + ip[0] * math.acos(prev_state[2])
+    y_next = prev_state[1] + ip[0] * math.sin(prev_state[2])
     theta_next = prev_state[2] + ip[1]
 
     return np.transpose(np.array([x_next, y_next, theta_next]))
