d518c254f13cfb1b10a7fd5bf773415b68ed1100
zsustc <843811748@qq.com>
2022-04-27 20:30:03 +0800

    Update task1.py

--- hw8/code/task1.py
+++ hw8/code/task1.py
@@ -3,8 +3,8 @@ import matplotlib.pylab as plt
 
 def log_inv_sensor_model(z,c):
     if c > z :
-        return np.log(0.6 / (1 - 0.6))
-    return np.log(0.3 / (1 - 0.3))
+        return #Todo
+    return #Todo
 
 measurements = [101, 82, 91, 112, 99, 151, 96, 85, 99, 105]
 l_m = np.zeros(21)
@@ -17,6 +17,6 @@ for z in measurements:
 
         l_m[i] += log_inv_sensor_model(z, m[i])
         
-p_m = 1-1/(1+np.exp(l_m))
+p_m = # Todo
 plt.plot(m,p_m)
 plt.show()
