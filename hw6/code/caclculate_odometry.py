4b562e02c5aca0ac33f7111df7a3120e127fc3d4
Han <744475118@qq.com>
2023-04-11 17:25:36 +0800

    Update caclculate_odometry.py

--- hw6/code/caclculate_odometry.py
+++ hw6/code/caclculate_odometry.py
@@ -125,7 +125,10 @@ def showPlots():
 
 if __name__ == "__main__":
 
-    _file = 'data/velocity/square_left.csv'
+    _file = 'data/velocity/square_left.csv'#All the .csv files are under the data 
+    
+    # -forward.csv, backward.csv - motion length 1.15m -left_full_turn.csv, right_full_turn.csv - full angle rotation -square_right.csv, square_left.csv - square side length = 1.15m
+    
 
     MIN_INT_16 = -32768
     MAX_INT_16 = 32767
